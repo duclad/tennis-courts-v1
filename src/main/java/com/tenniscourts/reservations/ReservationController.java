702d742d6772ec3fa9a4d6a15728c5d21fac5ca3
cr-ClaudiuDumitrescu <90391353+cr-ClaudiuDumitrescu@users.noreply.github.com>
2022-02-17 14:47:14 +0200

    reservations api implemented

--- src/main/java/com/tenniscourts/reservations/ReservationController.java
+++ src/main/java/com/tenniscourts/reservations/ReservationController.java
@@ -1,27 +1,62 @@
 package com.tenniscourts.reservations;
 
 import com.tenniscourts.config.BaseRestController;
-import lombok.AllArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController extends BaseRestController {
 
     private final ReservationService reservationService;
 
-    public ResponseEntity<Void> bookReservation(CreateReservationRequestDTO createReservationRequestDTO) {
+    @Operation(summary = "Create new reservation", tags = "reservations")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created")})
+    @PostMapping
+    public ResponseEntity<Void> bookReservation(@RequestBody CreateReservationRequestDTO createReservationRequestDTO) {
         return ResponseEntity.created(locationByEntity(reservationService.bookReservation(createReservationRequestDTO).getId())).build();
     }
 
-    public ResponseEntity<ReservationDTO> findReservation(Long reservationId) {
+
+    @Operation(summary = "Find a reservation by its id ", tags = "reservations")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success|OK", content = @Content(schema = @Schema(implementation = ReservationDTO.class)))})
+    @GetMapping("/{reservationId}")
+    public ResponseEntity<ReservationDTO> findReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.findReservation(reservationId));
     }
 
-    public ResponseEntity<ReservationDTO> cancelReservation(Long reservationId) {
+    @Operation(summary = "Cancel a reservation by its id ", tags = "reservations")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success|OK", content = @Content(schema = @Schema(implementation = ReservationDTO.class)))})
+    @DeleteMapping("/{reservationId}")
+    public ResponseEntity<ReservationDTO> cancelReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.cancelReservation(reservationId));
     }
 
-    public ResponseEntity<ReservationDTO> rescheduleReservation(Long reservationId, Long scheduleId) {
+    @Operation(summary = "Reschedule a reservation by its id ", tags = "reservations")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success|OK", content = @Content(schema = @Schema(implementation = ReservationDTO.class)))})
+    @PutMapping("{reservationId}")
+    public ResponseEntity<ReservationDTO> rescheduleReservation(@PathVariable Long reservationId,@RequestParam Long scheduleId) {
         return ResponseEntity.ok(reservationService.rescheduleReservation(reservationId, scheduleId));
     }
 }
--- src/main/java/com/tenniscourts/reservations/ReservationService.java
+++ src/main/java/com/tenniscourts/reservations/ReservationService.java
@@ -1,12 +1,22 @@
 package com.tenniscourts.reservations;
 
+import com.tenniscourts.exceptions.BusinessException;
 import com.tenniscourts.exceptions.EntityNotFoundException;
-import lombok.AllArgsConstructor;
+import com.tenniscourts.guests.Guest;
+import com.tenniscourts.guests.GuestRepository;
+import com.tenniscourts.schedules.Schedule;
+import com.tenniscourts.schedules.ScheduleRepository;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
@@ -14,10 +24,32 @@ public class ReservationService {
 
     private final ReservationRepository reservationRepository;
 
+    private final GuestRepository guestRepository;
+
+    private final ScheduleRepository scheduleRepository;
+
     private final ReservationMapper reservationMapper;
 
+    @Value("${reservation.deposit}")
+    private BigDecimal reservationDeposit;
+
+    @Transactional
     public ReservationDTO bookReservation(CreateReservationRequestDTO createReservationRequestDTO) {
-        throw new UnsupportedOperationException();
+        Guest guest = guestRepository.findById(createReservationRequestDTO.getGuestId()).orElseThrow(() -> new EntityNotFoundException("Guest not found!"));
+        Schedule schedule = scheduleRepository.findById(createReservationRequestDTO.getScheduleId()).orElseThrow(() -> new EntityNotFoundException("Schedule slot not found!"));
+        if (schedule.getReservations() != null && schedule.getReservations().stream()
+                .anyMatch(reservation -> ReservationStatus.RESCHEDULED == reservation.getReservationStatus() || ReservationStatus.READY_TO_PLAY == reservation.getReservationStatus())) {
+            throw new BusinessException("Schedule slot is already reserved");
+        }
+        Reservation reservation = new Reservation();
+        reservation.setValue(reservationDeposit);
+        reservation.setRefundValue(BigDecimal.ZERO);
+        reservation.setSchedule(schedule);
+        reservation.setGuest(guest);
+        reservation.setReservationStatus(ReservationStatus.READY_TO_PLAY);
+
+        reservationRepository.save(reservation);
+        return reservationMapper.map(reservation);
     }
 
     public ReservationDTO findReservation(Long reservationId) {
@@ -26,6 +58,7 @@ public class ReservationService {
         });
     }
 
+    @Transactional
     public ReservationDTO cancelReservation(Long reservationId) {
         return reservationMapper.map(this.cancel(reservationId));
     }
@@ -43,6 +76,7 @@ public class ReservationService {
         });
     }
 
+
     private Reservation updateReservation(Reservation reservation, BigDecimal refundValue, ReservationStatus status) {
         reservation.setReservationStatus(status);
         reservation.setValue(reservation.getValue().subtract(refundValue));
@@ -55,8 +89,8 @@ public class ReservationService {
         if (!ReservationStatus.READY_TO_PLAY.equals(reservation.getReservationStatus())) {
             throw new IllegalArgumentException("Cannot cancel/reschedule because it's not in ready to play status.");
         }
-
-        if (reservation.getSchedule().getStartDateTime().isBefore(LocalDateTime.now())) {
+        long minutes = ChronoUnit.MINUTES.between(LocalDateTime.now(), reservation.getSchedule().getStartDateTime());
+        if (minutes<=1) {
             throw new IllegalArgumentException("Can cancel/reschedule only future dates.");
         }
     }
@@ -66,13 +100,17 @@ public class ReservationService {
 
         if (hours >= 24) {
             return reservation.getValue();
+        } else if (hours>=12){
+            return reservation.getValue().multiply(new BigDecimal("0.25")).setScale(2, RoundingMode.HALF_UP);
+        }else if (hours>=2){
+            return reservation.getValue().multiply(new BigDecimal("0.50")).setScale(2, RoundingMode.HALF_UP);
         }
-
-        return BigDecimal.ZERO;
+        return reservation.getValue().multiply(new BigDecimal("0.75")).setScale(2, RoundingMode.HALF_UP);
     }
 
     /*TODO: This method actually not fully working, find a way to fix the issue when it's throwing the error:
             "Cannot reschedule to the same slot.*/
+    @Transactional
     public ReservationDTO rescheduleReservation(Long previousReservationId, Long scheduleId) {
         Reservation previousReservation = cancel(previousReservationId);
 
